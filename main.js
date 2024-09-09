3764f7c549e8ccc44b6699c4deea8e23a45237ee
Anurag Verma <mindreaders7557@gmail.com>
2024-09-09 17:21:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -198,10 +198,6 @@ document.querySelector('.btn').addEventListener('click', function(event) {
     if (screenWidth < 480) {
         document.querySelector('.contact__form').submit();
     } else {
-        const btn = event.currentTarget;
-        setTimeout(function() {
-           window.location.reload();
-        }, 5000);
         if (!btn.classList.contains('clicked')) {
             btn.classList.add('clicked');
         } else if (btn.classList.contains('clicked')) {
